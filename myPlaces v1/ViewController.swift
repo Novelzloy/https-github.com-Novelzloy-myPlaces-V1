534fcbf74d6c7f610b781c81747ccb55db18bafa
Novel <novel_90@mail.ru>
2021-01-05 11:49:56 +0300

    Initial Commit

---
+++ myPlaces v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myPlaces v1
+//
+//  Created by Роман on 05.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
