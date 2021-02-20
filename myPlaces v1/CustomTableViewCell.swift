a3405624153a3d37813ec6d94afe393eeb7a7fc3
Novel <novel_90@mail.ru>
2021-02-20 21:25:56 +0300

    This version program was add: - connect with custom class

---
+++ myPlaces v1/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  myPlaces v1
+//
+//  Created by Роман on 10.01.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var typeLable: UILabel!
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var nameLable: UILabel!
+}
